1c5fe1241dc845d6594dbbb02382416877c4b852
Shawn Xu <xusheng@62:7f:e3:74:dd:ac.home>
2025-05-09 21:24:01 -0400

    Update deployment configuration to handle frontend build

--- backend/server.js
+++ backend/server.js
@@ -115,10 +115,12 @@ app.head('/api/socket', (req, res) => {
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React app
   app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
+
+  // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 }
 
